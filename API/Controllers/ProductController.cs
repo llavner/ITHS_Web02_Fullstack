09c84b6e07d461c5409ca440a03f3e15a44bd20f
llavner <marcus.renvall@gmail.com>
2025-03-18 20:46:09 +0100

    Added controllers for the API

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,80 @@
+using Core.Entites;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController(IProductRepository productRepository) : ControllerBase
+{
+
+    private readonly IProductRepository _productRepository = productRepository;
+
+    // GET: api/<ProductController>
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var products = await _productRepository.GetAllAsync();
+        return Ok(products);
+    }
+
+    // GET api/<ProductController>/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+
+        if (product == null)
+            return NotFound();
+        else
+            return Ok(product);
+    }
+
+    // POST api/<ProductController>
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Product product)
+    {
+        if (string.IsNullOrWhiteSpace(product.Name))
+        {
+            return BadRequest(new { message = "Value can not be empty" });
+        }
+        
+        await _productRepository.AddAsync(product);
+
+        return Ok(new { message = $"Added Product {product.Name}", product });
+    }
+
+    // PUT api/<ProductController>/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, [FromBody] Product product)
+    {
+        if (id != product.Id)
+            return BadRequest(new { message = $"Id: {id} not found." });
+        else
+        {
+            await _productRepository.UpdateAsync(product);
+            return Ok(new { message = $"{product.Name} has been updated." });
+        }
+
+
+    }
+
+    // DELETE api/<ProductController>/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+
+        if (product == null)
+            return NotFound(new { message = "Product not found." });
+        else
+        {
+            await _productRepository.DeleteAsync(id);
+            return Ok(new { message = $"{product.Name} deleted." });
+
+        }
+
+    }
+}
+
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,48 @@
+using Core.Interfaces;
+using Data.DatabaseContext;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "CRUD API",
+        Description = "An ASP.NET Core Web API for managing products, customers and admin.",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Marcus Renvall",
+            Url = new Uri("https://github.com/llavner")
+        },
+        License = new OpenApiLicense
+        {
+            //Name = "None",
+            //Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
+// ConnectionString DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLconnection")));
+
+// Register Repository Service
+builder.Services.AddScoped<IProductRepository, ProductRepository>()
+                .AddScoped<ICustomerRepository, CustomerRepository>()
+                .AddScoped<ICatogoryRepository, CategoryRepository>()
+                .AddScoped<IOrderRepository, OrderRepository>();
+
+// API Controllers
+builder.Services.AddControllers();
+
+// HttpClient
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 

8705fafc14a40ad5618ffaa925ed3a5dd09969dd
llavner <marcus.renvall@gmail.com>
2025-03-18 20:34:13 +0100

    Added Interfaces

---
+++ Core/Interfaces/ICatogoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entites;
+
+namespace Core.Interfaces
+{
+    public interface ICatogoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task AddAsync(Category entity);
+        Task UpdateAsync(Category entity);
+        Task DeleteAsync(int id);
+
+    }
+}
---
+++ Core/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entites;
+
+namespace Core.Interfaces;
+
+public interface IOrderRepository
+{
+    Task<IEnumerable<Order>> GetAllAsync();
+    Task<Order> GetByIdAsync(int id);
+    Task AddAsync(Order entity);
+    Task UpdateAsync(Order entity);
+    Task DeleteAsync(int id);
+}
+

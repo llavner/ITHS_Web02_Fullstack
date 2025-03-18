9aa022dee9270ec3a1203a73515f8ae90cddede2
llavner <marcus.renvall@gmail.com>
2025-03-18 20:40:54 +0100

    Added DbContext for SQL

---
+++ Data/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.DatabaseContext;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetails> OrderDetails { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+}
+

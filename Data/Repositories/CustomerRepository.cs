9a1291586f6862d65b2fb0d761d02d1cf1e34261
llavner <marcus.renvall@gmail.com>
2025-03-18 20:41:11 +0100

    Added repository patterns

---
+++ Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,46 @@
+﻿using Core.Entites;
+using Core.Interfaces;
+using Data.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public class CustomerRepository(ApplicationDbContext context) : ICustomerRepository
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<IEnumerable<Customer>> GetAllAsync()
+    {
+        return await _context.Customers.ToListAsync();
+
+    }
+
+    public async Task<Customer> GetByIdAsync(int id)
+    {
+        return await _context.Customers.FindAsync(id);
+    }
+
+    public async Task AddAsync(Customer entity)
+    {
+        await _context.Customers.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Customer entity)
+    {
+        _context.Customers.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await _context.Customers.FindAsync(id);
+        if (entity != null)
+        {
+            _context.Customers.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+}
+

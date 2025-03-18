20039dfc416a80da7be128fdaaa2f1ecb00bdd36
llavner <marcus.renvall@gmail.com>
2025-03-18 20:33:28 +0100

    Added entity models

---
+++ Core/Entites/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entites;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public bool IsProductAvailable { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
+
+

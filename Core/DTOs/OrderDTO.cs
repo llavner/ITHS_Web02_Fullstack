acfec346c9786dbefda108a3a40f061ef2c1fd45
llavner <marcus.renvall@gmail.com>
2025-03-28 11:16:38 +0100

    Added loop in products.razor and updated DTOs with dataannotations. Updated Admin Tools

--- Core/DTOs/OrderDTO.cs
+++ Core/DTOs/OrderDTO.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Entites;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,14 @@ namespace Core.DTOs;
 public class OrderDTO
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "CustomerId is required.")]
     public int CustomerId { get; set; }
     public Customer Customer { get; set; }
+    [Required(ErrorMessage = "OrderId is required.")]
     public int OrderItemId { get; set; }
     public OrderItem OrderItem { get; set; }
     public DateTime OrderDate { get; set; }
+    [Required(ErrorMessage = "Status is required.")]
     public string Status { get; set; }
 
 }

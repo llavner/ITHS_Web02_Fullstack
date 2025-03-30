e8f7d8a090ef51a7a26754ba1fb794f31aed0df2
llavner <marcus.renvall@gmail.com>
2025-03-30 14:38:20 +0200

    Added DTOs for the Cart

---
+++ Core/DTOs/CartItemDTO.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs;
+
+public class CartItemDTO
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+    public int Quantity { get; set; }
+    public long Price { get; set; }
+
+}
+

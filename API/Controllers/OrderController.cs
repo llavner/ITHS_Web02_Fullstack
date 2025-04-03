49efe2ba42fc4e6c3f6e3084b308653c48c2da94
llavner <marcus.renvall@gmail.com>
2025-04-03 13:23:47 +0200

    Added OrderService

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
-﻿using Core.Entites;
+﻿using Core.DTOs;
+using Core.Entites;
+using Core.Extensions;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -34,46 +36,42 @@ public class OrderController(IOrderRepository orderRepository) : ControllerBase
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] Order order)
     {
-        if (string.IsNullOrWhiteSpace(order.Status))
-        {
-            return BadRequest(new { message = "Value can not be empty" });
-        }
-
+        
         await _orderRepository.AddAsync(order);
 
         return Ok(new { message = $"Added order {order.Id}", order });
     }
 
-    // PUT api/<OrderController>/5
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, [FromBody] Order order)
-    {
-        if (id != order.Id)
-            return BadRequest(new { message = $"Id: {id} not found." });
-        else
-        {
-            await _orderRepository.UpdateAsync(order);
-            return Ok(new { message = $"Order with Id: {order.Id} has been updated." });
-        }
+    //// PUT api/<OrderController>/5
+    //[HttpPut("{id}")]
+    //public async Task<IActionResult> Put(int id, [FromBody] Order order)
+    //{
+    //    if (id != order.Id)
+    //        return BadRequest(new { message = $"Id: {id} not found." });
+    //    else
+    //    {
+    //        await _orderRepository.UpdateAsync(order);
+    //        return Ok(new { message = $"Order with Id: {order.Id} has been updated." });
+    //    }
 
 
-    }
+    //}
 
-    // DELETE api/<OrderController>/5
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(int id)
-    {
-        var order = await _orderRepository.GetByIdAsync(id);
+    //// DELETE api/<OrderController>/5
+    //[HttpDelete("{id}")]
+    //public async Task<IActionResult> Delete(int id)
+    //{
+    //    var order = await _orderRepository.GetByIdAsync(id);
 
-        if (order == null)
-            return NotFound(new { message = "Order not found." });
-        else
-        {
-            await _orderRepository.DeleteAsync(id);
-            return Ok(new { message = $"Order with Id: {order.Id} deleted." });
+    //    if (order == null)
+    //        return NotFound(new { message = "Order not found." });
+    //    else
+    //    {
+    //        await _orderRepository.DeleteAsync(id);
+    //        return Ok(new { message = $"Order with Id: {order.Id} deleted." });
 
-        }
+    //    }
 
-    }
+    //}
 }
 

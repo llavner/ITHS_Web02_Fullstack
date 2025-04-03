2e0cba0abf6744bbe4e1f2f881e9ca49e98332cb
llavner <marcus.renvall@gmail.com>
2025-04-03 13:20:41 +0200

    Added Snackbar Services and ContentService to program.cs

--- Estore.Client/Program.cs
+++ Estore.Client/Program.cs
@@ -1,18 +1,30 @@
 using Estore.Client.Components;
 using MudBlazor.Services;
 using Core.Services;
+using MudBlazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add MudBlazor services
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+    config.SnackbarConfiguration.PreventDuplicates = true;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = false;
+    config.SnackbarConfiguration.VisibleStateDuration = 1000; // Duration in milliseconds
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
-builder.Services.AddScoped<ProductService>().AddScoped<UserService>().AddScoped<CategoryService>();
+builder.Services.AddScoped<ProductService>().AddScoped<UserService>().AddScoped<CategoryService>().AddScoped<OrderService>();
 builder.Services.AddHttpClient();
+builder.Services.AddSingleton<ContentService>();
 
 var app = builder.Build();
 

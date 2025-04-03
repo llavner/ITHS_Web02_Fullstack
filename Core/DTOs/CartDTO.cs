49c0e48b01591607c13297a6fe032b5e04eaf843
llavner <marcus.renvall@gmail.com>
2025-04-03 13:24:09 +0200

    Added methods to Cart class

--- Core/DTOs/CartDTO.cs
+++ Core/DTOs/CartDTO.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entites;
+using Core.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,29 @@ public class CartDTO
 {
     public int Id { get; set; }
     public ICollection<CartItemDTO> CartItems { get; set; } = [];
+    public void AddItem(ProductDTO item, IEnumerable<ProductDTO> products)
+    {
+        var product = products.FirstOrDefault(i => i.Id == item.Id) ?? throw new ArgumentException("Item not found");
+        
+        var existingItem = CartItems.FirstOrDefault(i => i.ProductId == item.Id);
+        if (existingItem is not null)
+        {
+            existingItem.Quantity += 1;
+        }
+        else
+        {
+            var newItem = product.ToCartItemDTO();
+            newItem.Quantity = 1;
+            CartItems.Add(newItem);
+        }
+    }
+
+    public void RemoveItem(ProductDTO item, IEnumerable<ProductDTO> products)
+    {
+        
+
+        
+    }
+
 }
 

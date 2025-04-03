ced021da248cbe0be497678bac6a00455c6ed9db
llavner <marcus.renvall@gmail.com>
2025-04-03 13:26:24 +0200

    Deleted unused methods in ProductService class

--- Core/Services/ProductService.cs
+++ Core/Services/ProductService.cs
@@ -16,29 +16,7 @@ public class ProductService(HttpClient http)
     {
         var products = await _http.GetFromJsonAsync<List<Product>>("https://localhost:7120/api/Product");
 
-        return products.Select(p => p.ToProductDTO()); // Flytta till Controllern?
-
-    }
-
-    public async Task<IEnumerable<ProductDTO>> GetCDAsync()
-    {
-        var products = await _http.GetFromJsonAsync<List<Product>>("https://localhost:7120/api/Product");
-
-        return products.Where(c => c.Category.Name == "CD").Select(p => p.ToProductDTO());
-
-    }
-    public async Task<IEnumerable<ProductDTO>> GetVinylAsync()
-    {
-        var products = await _http.GetFromJsonAsync<List<Product>>("https://localhost:7120/api/Product");
-
-        return products.Where(c => c.Category.Name == "Vinyl").Select(p => p.ToProductDTO());
-
-    }
-    public async Task<IEnumerable<ProductDTO>> GetCasetteAsync()
-    {
-        var products = await _http.GetFromJsonAsync<List<Product>>("https://localhost:7120/api/Product");
-
-        return products.Where(c => c.Category.Name == "Casette").Select(p => p.ToProductDTO());
+        return products.Select(p => p.ToProductDTO());
 
     }
 

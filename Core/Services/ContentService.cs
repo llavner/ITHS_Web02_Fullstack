5ff23fbe16242ba74aa360a95a3807475b614e91
llavner <marcus.renvall@gmail.com>
2025-04-03 13:25:33 +0200

    Added ContentService-class

---
+++ Core/Services/ContentService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services;
+
+public class ContentService
+{
+    private string selectedContent = "Products";
+
+    public event Action? OnChange;
+
+    private async void NotifyStateChanged()
+    {
+        if (OnChange != null)
+        {
+            await Task.Run(() => OnChange.Invoke());
+        }
+    }
+    public string SelectedContent
+    {
+        get => selectedContent;
+        set
+        {
+            selectedContent = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public void SelectContent(string option)
+    {
+        SelectedContent = option;
+    }
+}
+
